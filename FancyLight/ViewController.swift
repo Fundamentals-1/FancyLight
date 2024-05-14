1aed24312cf91db28536ced83490c60d489d9c22
Brian Foutty <brian@swiftteacher.org>
2024-05-14 12:19:23 -0400

    App is fully functional and ready for teaching. Going to add swipe functionality next.

--- FancyLight/ViewController.swift
+++ FancyLight/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet weak var lightButton: UIButton!
+    
+    // MARK: - Instance Properties
+    var lightOn = true
 
+    
+    // MARK: - VDL
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
-
-
+    
+    // MARK: - IB Actions
+    @IBAction func lightButtonTapped(_ sender: Any) {
+        //lightOn = !lightOn
+        lightOn.toggle()
+        updateUI()
+    }
+    
+    
+    // MARK: - Instance Methods
+    func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
+            lightButton.setTitleColor(.black, for: .normal)
+        } else {
+            view.backgroundColor = .black
+            lightButton.setTitle("On", for: .normal)
+            lightButton.setTitleColor(.white, for: .normal)
+        }
+    }
 }
 
